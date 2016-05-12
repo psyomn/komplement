6a1847c812fa06e715854645bcbd25dd6849de90
psyomn <lethaljellybean@gmail.com>
2016-05-11 21:05:24 -0400

    Add simple specs

--- spec/komplement_spec.rb
+++ spec/komplement_spec.rb
@@ -5,4 +5,45 @@ describe Komplement do
   it "should have a VERSION constant" do
     expect(subject.const_get('VERSION')).to_not be_empty
   end
+
+  describe Base do
+    it "should detect common html elements" do
+      ret = Komplement::Base
+              .new
+              .with_ignored(Komplement::HTML_ELEMENTS)
+              .in_filetypes(['html'])
+              .in_dirs(['./spec/files/'])
+              .find_offenses
+
+      expect(ret).to_not be_empty
+      expect(ret['./spec/files/bad_html.html']).to eq(['potato'])
+    end
+
+    it "should detect everything if no exclusion list is given" do
+      ret = Komplement::Base
+              .new
+              .with_ignored([])
+              .in_filetypes(['html'])
+              .in_dirs(['./spec/files/'])
+              .find_offenses
+
+      expect(ret).to_not be_empty
+
+      expect(ret['./spec/files/bad_html.html']).to eq([
+        "html", "text", "head", "text", "text", "text", "text",
+        "potato", "text", "h1", "body", "text", "html", "document"])
+    end
+
+    it "should ignore custom elements if specified so" do
+      ret = Komplement::Base
+              .new
+              .with_ignored(Komplement::HTML_ELEMENTS)
+              .with_ignored(['potato'])
+              .in_filetypes(['html'])
+              .in_dirs(['./spec/files/'])
+              .find_offenses
+
+      expect(ret).to be_empty
+    end
+  end
 end
